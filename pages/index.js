070128a550cbe80596555f5da3717d414cdc12b8
John Canero <johncanero.official@gmail.com>
2022-09-06 00:14:23 +0800

    Head Title

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+        <Head>
+          <title>WebDev News</title>
+          <meta name='keywords' content='web development, programming'/>
+        </Head>
         <h1>Welcome to Next</h1>
     </div>
   )
