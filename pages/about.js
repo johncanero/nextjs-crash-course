a85b3304b5de5dc727be91b467995266ec2c7b4b
John Canero <johncanero.official@gmail.com>
2022-09-06 07:42:59 +0800

    Layout framework and CSS, About Content

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Head from 'next/head'
 
 const about = () => {
   return (
     <div>
+        <Head>
+          <title>About</title>
+        </Head>
         <h1>About</h1>
     </div>
   )
