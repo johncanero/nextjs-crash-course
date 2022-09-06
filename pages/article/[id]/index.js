0ee241e5a201b85f2f860df07390fc96e935865b
John Canero <johncanero.official@gmail.com>
2022-09-06 18:36:52 +0800

    use Router

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+
+const article = () => {
+
+  const router = useRouter()
+  const {id} = router.query
+
+  return (
+    <div>
+        This is an article {id}
+    </div>
+  )
+}
+
+export default article
