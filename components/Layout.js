3b1df99512993fe2530f1a74638d2a71b3b520ed
John Canero <johncanero.official@gmail.com>
2022-09-06 09:44:28 +0800

    Header Content Styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
+import Header from './Header'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({children}) => {
@@ -8,6 +9,7 @@ const Layout = ({children}) => {
         <Nav />
         <div className={styles.container}>
             <main className={styles.main}>
+                <Header />
                 {children}
             </main>
        </div>
