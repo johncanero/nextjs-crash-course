3e4e090e14413e70f824713dfb8907dcf1ab80fc
John Canero <johncanero.official@gmail.com>
2022-09-06 15:10:15 +0800

    Article Route

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
 
-export default function Home() {
+export default function Home({articles}) {
+  // console.log(articles)
   return (
     <div>
         <Head>
           <title>WebDev News</title>
           <meta name='keywords' content='web development, programming'/>
         </Head>
-        <h1>Welcome to Next</h1>
+
+        <ArticleList articles={articles} />
     </div>
   )
+}
+
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
