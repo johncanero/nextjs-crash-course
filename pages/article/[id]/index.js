b27356894b1c42ed344e524afedc4c958393523b
John Canero <johncanero.official@gmail.com>
2022-09-07 12:35:57 +0800

    Article Get Server Side Props

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 
-const article = () => {
+const article = ({article}) => {
 
-  const router = useRouter()
-  const {id} = router.query
+  // const router = useRouter()
+  // const {id} = router.query
 
   return (
-    <div>
-        This is an article {id}
-    </div>
+    <>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br />
+        <Link href='/'>Go Back</Link>
+    </>
   )
 }
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+    const article = await res.json()
+
+    return {
+      props: {
+        article,
+      },
+    }
+  }
+
 
 export default article
